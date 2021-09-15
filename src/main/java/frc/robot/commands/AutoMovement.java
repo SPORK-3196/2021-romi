283108805b0517dcbfc38bd0d21f77c825c71e96
PR0C355 <tumicooll@gmail.com>
2021-09-14 23:33:10 -0400

    Removed TImedPause, now uses built in WaitCommand

--- src/main/java/frc/robot/commands/AutoMovement.java
+++ src/main/java/frc/robot/commands/AutoMovement.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.RomiDrivetrain;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 // A complex auto command that drives forward, 
 
@@ -17,11 +18,14 @@ public class AutoMovement extends SequentialCommandGroup{
     public AutoMovement(RomiDrivetrain drivetrain){
         addCommands(
             //Drive forward the specified amount of time
-            new DriveForwardTimed(drivetrain, 1.0, -0.6), 
-            new TurnRight(drivetrain, -0.6),
-            new TimedPause(1.0),
-            new TurnLeft(drivetrain, -0.6),
-            new DriveBackwardsTimed(drivetrain, 1.0, -0.6)
+            new DriveForwardTimed(drivetrain, 5.0, -0.6), 
+            new WaitCommand(0.5),
+            new TurnRight(drivetrain, -0.7),
+            new WaitCommand(0.5),
+            new DriveForwardTimed(drivetrain, 10.0, -0.6), 
+            new WaitCommand(0.5),
+            new TurnRight(drivetrain, -0.7),
+            new DriveForwardTimed(drivetrain, 5.0, -0.6)
             //DriveForwardTimed command using the drivetrain object m_romiDrivetrain and with a power -0.6, for 1 second
 
         );
